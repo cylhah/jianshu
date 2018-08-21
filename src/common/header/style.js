087567a729c4bbf1b64e8d56e2dc523f0e80a7cf
cylhah <951045989@qq.com>
2018-08-21 10:06:23 +0800

    1

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
-import logo from '../../statics/logo.png'
+import logoPic from '../../statics/logo.png'
 
 export const HeaderWrapper = styled.div`
     position: relative;
     height: 58px;
     padding: 0 50px;
 `
+
+export const Logo 
