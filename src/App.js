940694bbe4b10eaf06f12b04076b7fdfb887225e
cylhah <37016501+cylhah@users.noreply.github.com>
2019-07-24 10:40:49 +0800

    go

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './common/header/Header';
-import { BrowserRouter,Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import Home from './pages/home';
@@ -12,16 +12,16 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <BrowserRouter>
-            <div>
-              <Header></Header>
-              <Route path='/' exact component={Home}></Route>
-              <Route path='/login' exact component={Login}></Route>
-              <Route path='/write' exact component={Write}></Route>
-              <Route path='/detail/:id' exact component={Detail}></Route>
-            </div>
-          </BrowserRouter>
-      </Provider>  
+        <BrowserRouter>
+          <div>
+            <Header></Header>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
+            <Route path='/write' exact component={Write}></Route>
+            <Route path='/detail/:id' exact component={Detail}></Route>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
